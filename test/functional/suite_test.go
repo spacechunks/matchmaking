2589bea0ce0144ebdc5dbf499717157b3f44fcd8
yannic rieger <10352239+freggy@users.noreply.github.com>
2026-06-10 18:28:07 +0200

    add more tests and run them in parallel (#1)

--- test/functional/suite_test.go
+++ test/functional/suite_test.go
@@ -2,6 +2,7 @@ package functional
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"net"
 	"testing"
@@ -18,40 +19,37 @@ import (
 )
 
 var config = server.Config{
-	ListeAddr:                            "localhost:60679",
 	MatchInterval:                        1 * time.Second,
 	AllocateInstanceForPendingMatchAfter: 3 * time.Second,
 }
-
-var serverCtx, cancel = context.WithCancel(context.Background())
 
 func TestFunctional(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Functional Suite")
 }
 
-var _ = BeforeSuite(func() {
-	var (
-		logger = slog.New(slog.NewTextHandler(GinkgoWriter, nil))
-		serv   = server.New(logger, config, matchmaking.NewStore[matchmaking.Ticket]())
-	)
-
-	go func() {
-		err := serv.Run(serverCtx)
-		Expect(err).NotTo(HaveOccurred())
-	}()
-})
-
-var _ = AfterSuite(func() {
-	cancel()
-})
-
-var _ = Describe("test matchmaking", func() {
+var _ = Describe("matchmaking", func() {
 	var client mmv1alpha1.MatchmakingServiceClient
+	serverCtx, serverCancel := context.WithCancel(context.Background())
 
 	BeforeEach(func(ctx context.Context) {
+		port := 60000 + GinkgoParallelProcess()
+		addr := fmt.Sprintf("localhost:%d", port)
+
+		config.ListeAddr = addr
+
+		var (
+			logger = slog.New(slog.NewTextHandler(GinkgoWriter, nil))
+			serv   = server.New(logger, config, matchmaking.NewStore[matchmaking.Ticket]())
+		)
+
+		go func() {
+			err := serv.Run(serverCtx)
+			Expect(err).NotTo(HaveOccurred())
+		}()
+
 		Eventually(func() error {
-			_, err := net.DialTimeout("tcp", "localhost:60679", 10*time.Second)
+			_, err := net.DialTimeout("tcp", addr, 10*time.Second)
 			return err
 		}).WithTimeout(10 * time.Second).Should(Succeed())
 
@@ -59,10 +57,10 @@ var _ = Describe("test matchmaking", func() {
 		Expect(err).NotTo(HaveOccurred())
 
 		client = mmv1alpha1.NewMatchmakingServiceClient(grpcClient)
+	})
 
-		// remove all tickets, so we have a clean state for every test
-		_, err = client.RemoveAllTickets(ctx, &mmv1alpha1.RemoveAllTicketsRequest{})
-		Expect(err).NotTo(HaveOccurred())
+	AfterEach(func(ctx context.Context) {
+		serverCancel()
 	})
 
 	It("matches two tickets of same flavor without delay", func(ctx SpecContext) {
@@ -83,10 +81,218 @@ var _ = Describe("test matchmaking", func() {
 			g.Expect(updated1.Assignment.InstanceId).To(Equal(updated2.Assignment.InstanceId))
 		}).WithTimeout(10 * time.Second).Should(Succeed())
 	})
+
+	It("matches multiple tickets of same flavor with delay", func(ctx SpecContext) {
+		var (
+			flavorID = uuid.NewString()
+			ticket1  = createAndActivateTicket(ctx, client, flavorID, 1)
+		)
+
+		time.Sleep(1 * time.Second)
+
+		ticket2 := createAndActivateTicket(ctx, client, flavorID, 1)
+
+		time.Sleep(1 * time.Second)
+
+		ticket3 := createAndActivateTicket(ctx, client, flavorID, 1)
+
+		Eventually(func(g Gomega) {
+			var (
+				updated1 = fetchTicket(ctx, client, ticket1.Id)
+				updated2 = fetchTicket(ctx, client, ticket2.Id)
+				updated3 = fetchTicket(ctx, client, ticket3.Id)
+			)
+
+			g.Expect(updated1.Assignment).NotTo(BeNil())
+			g.Expect(updated2.Assignment).NotTo(BeNil())
+			g.Expect(updated3.Assignment).NotTo(BeNil())
+
+			insID := updated1.Assignment.InstanceId
+			g.Expect(updated2.Assignment.InstanceId).To(Equal(insID))
+			g.Expect(updated3.Assignment.InstanceId).To(Equal(insID))
+		}).WithTimeout(10 * time.Second).Should(Succeed())
+	})
+
+	It("matches multiple tickets of different flavors", func(ctx SpecContext) {
+		var (
+			flavorID1 = uuid.NewString()
+			ticket1   = createAndActivateTicket(ctx, client, flavorID1, 1)
+			ticket2   = createAndActivateTicket(ctx, client, flavorID1, 2)
+
+			flavorID2 = uuid.NewString()
+			ticket3   = createAndActivateTicket(ctx, client, flavorID2, 1)
+			ticket4   = createAndActivateTicket(ctx, client, flavorID2, 2)
+		)
+
+		Eventually(func(g Gomega) {
+			var (
+				updated1 = fetchTicket(ctx, client, ticket1.Id)
+				updated2 = fetchTicket(ctx, client, ticket2.Id)
+				updated3 = fetchTicket(ctx, client, ticket3.Id)
+				updated4 = fetchTicket(ctx, client, ticket4.Id)
+			)
+
+			g.Expect(updated1.Assignment).NotTo(BeNil())
+			g.Expect(updated2.Assignment).NotTo(BeNil())
+			g.Expect(updated3.Assignment).NotTo(BeNil())
+			g.Expect(updated4.Assignment).NotTo(BeNil())
+
+			g.Expect(updated1.Assignment.InstanceId).To(Equal(updated2.Assignment.InstanceId))
+			g.Expect(updated3.Assignment.InstanceId).To(Equal(updated4.Assignment.InstanceId))
+
+			// make sure they do not equal one another, these should be two distinct matches
+			g.Expect(updated1.Assignment.InstanceId).NotTo(Equal(updated3.Assignment.InstanceId))
+			g.Expect(updated4.Assignment.InstanceId).NotTo(Equal(updated2.Assignment.InstanceId))
+		}).WithTimeout(10 * time.Second).Should(Succeed())
+	})
+
+	It("does not match tickets with different flavor ids", func(ctx SpecContext) {
+		var (
+			flavorID1 = uuid.NewString()
+			flavorID2 = uuid.NewString()
+			ticket1   = createAndActivateTicket(ctx, client, flavorID1, 1)
+			ticket2   = createAndActivateTicket(ctx, client, flavorID2, 2)
+		)
+
+		Consistently(func(g Gomega) {
+			var (
+				updated1 = fetchTicket(ctx, client, ticket1.Id)
+				updated2 = fetchTicket(ctx, client, ticket2.Id)
+			)
+
+			g.Expect(updated1.Assignment).To(BeNil())
+			g.Expect(updated2.Assignment).To(BeNil())
+		}, "5s").Should(Succeed())
+	})
+
+	It("should not match deactivated tickets", func(ctx SpecContext) {
+		var (
+			flavorID = uuid.NewString()
+			ticket1  = createAndActivateTicket(ctx, client, flavorID, 1)
+			ticket2  = createTicket(ctx, client, flavorID, 20)
+			ticket3  = createTicket(ctx, client, flavorID, 10)
+		)
+
+		Consistently(func(g Gomega) {
+			var (
+				updated1 = fetchTicket(ctx, client, ticket1.Id)
+				updated2 = fetchTicket(ctx, client, ticket2.Id)
+				updated3 = fetchTicket(ctx, client, ticket3.Id)
+			)
+
+			g.Expect(updated1.Assignment).To(BeNil())
+			g.Expect(updated2.Assignment).To(BeNil())
+			g.Expect(updated3.Assignment).To(BeNil())
+		}, "5s").Should(Succeed())
+	})
+
+	It("matches a ticket that has been activated", func(ctx SpecContext) {
+		var (
+			flavorID = uuid.NewString()
+			ticket1  = createAndActivateTicket(ctx, client, flavorID, 1)
+			ticket2  = createTicket(ctx, client, flavorID, 2)
+		)
+
+		time.Sleep(3 * time.Second)
+
+		activateTicket(ctx, client, ticket2.Id)
+
+		Eventually(func(g Gomega) {
+			var (
+				updated1 = fetchTicket(ctx, client, ticket1.Id)
+				updated2 = fetchTicket(ctx, client, ticket2.Id)
+			)
+
+			g.Expect(updated1.Assignment).NotTo(BeNil())
+			g.Expect(updated2.Assignment).NotTo(BeNil())
+			g.Expect(updated1.Assignment.InstanceId).To(Equal(updated2.Assignment.InstanceId))
+		}).WithTimeout(10 * time.Second).Should(Succeed())
+	})
+
+	It("creates 2 different matches when combined player count is greater than maxPlayer", func(ctx SpecContext) {
+		var (
+			flavorID = uuid.NewString()
+			ticket1  = createAndActivateTicket(ctx, client, flavorID, 9)
+			ticket2  = createAndActivateTicket(ctx, client, flavorID, 3)
+			ticket3  = createAndActivateTicket(ctx, client, flavorID, 3)
+		)
+
+		Eventually(func(g Gomega) {
+			var (
+				updated1 = fetchTicket(ctx, client, ticket1.Id)
+				updated2 = fetchTicket(ctx, client, ticket2.Id)
+				updated3 = fetchTicket(ctx, client, ticket3.Id)
+			)
+
+			g.Expect(updated1.Assignment).NotTo(BeNil())
+			g.Expect(updated2.Assignment).NotTo(BeNil())
+			g.Expect(updated3.Assignment).NotTo(BeNil())
+
+			// ticket1 is a separate match
+			g.Expect(updated1.Assignment.InstanceId).NotTo(Equal(updated2.Assignment.InstanceId))
+
+			// ticket2 and ticket3 should be matched together
+			g.Expect(updated2.Assignment.InstanceId).To(Equal(updated3.Assignment.InstanceId))
+
+		}, "10s").Should(Succeed())
+	})
+
+	It("creates a match for a ticket that has maxPlayers while others wait", func(ctx SpecContext) {
+		var (
+			flavorID = uuid.NewString()
+			ticket1  = createAndActivateTicket(ctx, client, flavorID, 10)
+			ticket2  = createAndActivateTicket(ctx, client, flavorID, 1)
+			ticket3  = createAndActivateTicket(ctx, client, flavorID, 1)
+		)
+
+		Eventually(func(g Gomega) {
+			updated := fetchTicket(ctx, client, ticket1.Id)
+			g.Expect(updated.Assignment).NotTo(BeNil())
+		}).WithTimeout(10 * time.Second).Should(Succeed())
+
+		Consistently(func(g Gomega) {
+			var (
+				updated2 = fetchTicket(ctx, client, ticket2.Id)
+				updated3 = fetchTicket(ctx, client, ticket3.Id)
+			)
+			g.Expect(updated2.Assignment).To(BeNil())
+			g.Expect(updated3.Assignment).To(BeNil())
+		}, "5s").Should(Succeed())
+	})
+
+	It("should not match tickets if combined playerCounts cannot reach minPlayers", func(ctx SpecContext) {
+		var (
+			flavorID = uuid.NewString()
+			ticket1  = createAndActivateTicket(ctx, client, flavorID, 1)
+			ticket2  = createAndActivateTicket(ctx, client, flavorID, 1)
+		)
+
+		Consistently(func(g Gomega) {
+			var (
+				updated1 = fetchTicket(ctx, client, ticket1.Id)
+				updated2 = fetchTicket(ctx, client, ticket2.Id)
+			)
+
+			g.Expect(updated1.Assignment).To(BeNil())
+			g.Expect(updated2.Assignment).To(BeNil())
+		}, "5s").Should(Succeed())
+	})
+
+	// TODO: test that after min players change old run with the old flavor version and new ones with the new
 })
 
 func createAndActivateTicket(
 	ctx context.Context,
+	client mmv1alpha1.MatchmakingServiceClient,
+	flavorId string,
+	playerCount uint32,
+) *mmv1alpha1.Ticket {
+	t := createTicket(ctx, client, flavorId, playerCount)
+	activateTicket(ctx, client, t.Id)
+	return t
+}
+
+func createTicket(ctx context.Context,
 	client mmv1alpha1.MatchmakingServiceClient,
 	flavorId string,
 	playerCount uint32,
@@ -97,12 +303,14 @@ func createAndActivateTicket(
 	})
 	Expect(err).NotTo(HaveOccurred())
 
-	_, err = client.ActivateTicket(ctx, &mmv1alpha1.ActivateTicketRequest{
-		TicketId: createResp.Ticket.Id,
+	return createResp.Ticket
+}
+
+func activateTicket(ctx context.Context, client mmv1alpha1.MatchmakingServiceClient, ticketID string) {
+	_, err := client.ActivateTicket(ctx, &mmv1alpha1.ActivateTicketRequest{
+		TicketId: ticketID,
 	})
 	Expect(err).NotTo(HaveOccurred())
-
-	return createResp.Ticket
 }
 
 func fetchTicket(ctx context.Context, client mmv1alpha1.MatchmakingServiceClient, ticketID string) *mmv1alpha1.Ticket {
